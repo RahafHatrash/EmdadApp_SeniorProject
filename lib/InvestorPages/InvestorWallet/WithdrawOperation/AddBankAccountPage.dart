055bed975542f659dfed0a205acb59ed3c530cb0
rahat <r.a.hatrash2001@gmail.com>
2024-12-01 18:39:15 +0300

    changes by rahaf

--- lib/InvestorPages/InvestorWallet/WithdrawOperation/AddBankAccountPage.dart
+++ lib/InvestorPages/InvestorWallet/WithdrawOperation/AddBankAccountPage.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-/*class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bank Account Addition',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: AddBankAccountPage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}*/
 
 class AddBankAccountPage extends StatefulWidget {
   @override
